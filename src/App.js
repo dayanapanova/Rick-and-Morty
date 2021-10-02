0176cb5d7db232c617a60239bcc4ac123dec0b82
Dayana Panova <dzopino@abv.bg>
2021-10-02 18:46:28 +0300

    setting react router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import Navigation from './components/Navigation';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 import './App.css';
-
+import Characters from './components/Characters';
+import Episodes from './components/Episodes';
+import Home from './components/Home'
 function App() {
   return (
-    <div className="App">
-      <Navigation />
-    </div>
+    <Router>
+      <div className="App">
+        <Navigation />
+      </div>
+      <Switch>
+        <Route exact path='/' component={Home}></Route>
+        <Route exact path='/episodes' component={Episodes}></Route>
+        <Route exact path='/characters' component={Characters}></Route>
+      </Switch>
+    </Router>
   );
 }
 
--- src/assets/CharacterIcon.js
+++ src/assets/CharacterIcon.js
@@ -2,7 +2,6 @@ import React from "react";
 
 export default (props) => (
     <svg
-        width="40"
         xmlns="http://www.w3.org/2000/svg"
         x="0"
         y="0"
