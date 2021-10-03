ae5bd6f43524993e9721b8e857fa8b7bf9cb149e
Dayana Panova <dzopino@abv.bg>
2021-10-03 20:29:45 +0300

    filling an react component with data

--- src/components/SingleCharacter.js
+++ src/components/SingleCharacter.js
@@ -1,13 +1,29 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 import axios from 'axios';
+
 const SingleCharacter = () => {
+    const rickAndMortyUrl = 'https://rickandmortyapi.com/api/character';
+    const [userData, setUserData] = useState([]);;
+
+    useEffect(() => {
+        getData();
+    }, []);
+
+    const getData = async () => {
+        const response = await axios.get(rickAndMortyUrl);
+        console.log(response?.data?.results);
+        setUserData(response?.data?.results);
+    };
     return (
         <div className="character-holder">
-            <div className="character-holder-image"></div>
-            <div className="chracter-holder-info">
-                <div>Title</div>
-                <div>Info</div>
-                <div>Status</div>
+            <img className="character-holder-image" alt=''>
+                {userData.image}
+            </img>
+            <div className="character-holder-info">
+                <div>{userData.name}</div>
+                <div>{userData.species}</div>
+                <div>{userData.status}</div>
             </div>
         </div>
     )
